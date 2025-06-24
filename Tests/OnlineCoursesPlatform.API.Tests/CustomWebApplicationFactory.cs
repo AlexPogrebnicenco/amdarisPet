bd9fd325a425176d3b635eee2d7c35467d89f551
Alexandru Pogrebnicenco <alexpogrebnicenco@gmail.com>
2025-06-24 16:01:54 +0300

    Integration tests for AuthController added

---
+++ Tests/OnlineCoursesPlatform.API.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace OnlineCoursesPlatform.API.Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+    }
+}
---
+++ Tests/OnlineCoursesPlatform.API.Tests/IntegrationTestBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace OnlineCoursesPlatform.API.Tests
+{
+    public class IntegrationTestBase : IClassFixture<CustomWebApplicationFactory>
+    {
+        protected readonly HttpClient _client;
+        protected readonly CustomWebApplicationFactory Factory;
+
+        public IntegrationTestBase(CustomWebApplicationFactory factory)
+        {
+            Factory = factory; // сохраняем Factory
+            _client = Factory.CreateClient();
+        }
+
+        protected StringContent GetPayload(object obj)
+        {
+            return new StringContent(JsonSerializer.Serialize(obj), Encoding.UTF8, "application/json");
+        }
+    }
+}
